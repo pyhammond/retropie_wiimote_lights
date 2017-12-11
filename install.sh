6b70435d34e572438c1f285078d3ee03279873d0
Peter Y. Hammond <peter@utahrealestate.com>
2017-12-10 21:07:26 -0700

    Added my custom controller config for RetroArch

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cp Nintendo\ Wii\ Remote\ Pro\ Controller.cfg /opt/retropie/configs/all/retroarch/autoconfig/
 sudo cp wii-controller.service /etc/systemd/system/
 sudo systemctl enable wii-controller.service
 sudo systemctl start wii-controller.service
