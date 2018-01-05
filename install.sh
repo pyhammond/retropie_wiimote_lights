d93bd25a701827ca3593b1254f6b9102dfc10a78
Peter Y. Hammond <peter@utahrealestate.com>
2018-01-05 12:03:11 -0700

    made sure to backup the old config files first before replacing them

--- install.sh
+++ install.sh
@@ -2,10 +2,17 @@
 
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# First backup some old configs that we are going to replace
+cp /opt/retropie/configs/all/retroarch/autoconfig/Nintendo\ Wii\ Remote\ Pro\ Controller.cfg /opt/retropie/configs/all/retroarch/autoconfig/Nintendo\ Wii\ Remote\ Pro\ Controller.cfg.bak \; 2>/dev/null
+cp /opt/retropie/configs/daphne/dapinput.ini configs/dapinput.ini /opt/retropie/configs/daphne/dapinput.ini.bak \; 2>/dev/null
+cp /opt/retropie/configs/n64/InputAutoCfg.ini /opt/retropie/configs/n64/InputAutoCfg.ini.bak \; 2>/dev/null
+cp /opt/retropie/configs/n64/mupen64plus.cfg /opt/retropie/configs/n64/mupen64plus.cfg.bak \; 2>/dev/null
+
 # Configure the controllers for several emulators to use our custom configs
-cp configs/Nintendo\ Wii\ Remote\ Pro\ Controller.cfg /opt/retropie/configs/all/retroarch/autoconfig/
-cp configs/dapinput.ini /opt/retropie/configs/daphne/
-pushd /opt/retropie/configs/n64 && (rm -f InputAutoCfg.ini*; cp mupen64plus.cfg mupen64plus.cfg.bak; sed 's/Joy Mapping Stop = ""/Joy Mapping Stop = "J0B8\/B9"/g;s/Joy Mapping Save State = ""/Joy Mapping Save State = "J0B8\/B5"/g;s/Joy Mapping Load State = ""/Joy Mapping Load State = "J0B8\/B4"/g' mupen64plus.cfg.rp-dist > mupen64plus.cfg); popd
+cp configs/Nintendo\ Wii\ Remote\ Pro\ Controller.cfg /opt/retropie/configs/all/retroarch/autoconfig/Nintendo\ Wii\ Remote\ Pro\ Controller.cfg
+cp configs/dapinput.ini /opt/retropie/configs/daphne/dapinput.ini
+cp configs/InputAutoCfg.ini /opt/retropie/configs/n64/InputAutoCfg.ini
+pushd /opt/retropie/configs/n64 && (sed 's/Joy Mapping Stop =.*/Joy Mapping Stop = "J0B8\/B9"/g;s/Joy Mapping Save State =.*/Joy Mapping Save State = "J0B8\/B5"/g;s/Joy Mapping Load State =.*/Joy Mapping Load State = "J0B8\/B4"/g' mupen64plus.cfg.rp-dist > mupen64plus.cfg); popd
 
 # Install the service to light up the wiimote lights correctly
 sudo cp wii-controller.service /etc/systemd/system/
